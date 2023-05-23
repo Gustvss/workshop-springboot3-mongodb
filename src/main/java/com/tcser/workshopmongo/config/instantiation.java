741dda295971748a1e1bca598352ccf32de553cc
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-05-23 13:31:58 -0300

    Operation to instantiantion the database

---
+++ src/main/java/com/tcser/workshopmongo/config/instantiation.java
@@ -0,0 +1,28 @@
+package com.tcser.workshopmongo.config;
+
+import com.tcser.workshopmongo.entities.User;
+import com.tcser.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... arg0) throws Exception{
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
