bd18336b1e7e26d51af5ed0c875c1d0085c6b1eb
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-05-23 14:49:35 -0300

    Insert User with POST method

--- src/main/java/com/tcser/workshopmongo/services/UserService.java
+++ src/main/java/com/tcser/workshopmongo/services/UserService.java
@@ -1,5 +1,6 @@
 package com.tcser.workshopmongo.services;
 
+import com.tcser.workshopmongo.dto.UserDTO;
 import com.tcser.workshopmongo.entities.User;
 import com.tcser.workshopmongo.repositories.UserRepository;
 import com.tcser.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,6 +24,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public User insert(User obj) {
+       return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
