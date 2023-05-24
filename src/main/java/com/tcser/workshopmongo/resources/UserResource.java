83eac6cc5c9b2fd7084f83c0eeecd349abdd9c93
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-05-24 08:20:08 -0300

    Endpoint to return a User Posts

--- src/main/java/com/tcser/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tcser/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.tcser.workshopmongo.resources;
 
 import com.tcser.workshopmongo.dto.UserDTO;
+import com.tcser.workshopmongo.entities.Post;
 import com.tcser.workshopmongo.entities.User;
 import com.tcser.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,5 +56,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(id, obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
