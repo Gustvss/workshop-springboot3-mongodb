b60dd8f7d93838b97ce4ae4822e540ba3b660110
Gustavo Sant'Ana <gustvss.dev@gmail.com>
2023-05-24 09:13:39 -0300

    Simple query with Query Methods

--- src/main/java/com/tcser/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/tcser/workshopmongo/repositories/PostRepository.java
@@ -5,7 +5,10 @@ import com.tcser.workshopmongo.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     Post getReferenceById(String id);
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/tcser/workshopmongo/resources/PostResource.java
+++ src/main/java/com/tcser/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.tcser.workshopmongo.resources;
 import com.tcser.workshopmongo.dto.UserDTO;
 import com.tcser.workshopmongo.entities.Post;
 import com.tcser.workshopmongo.entities.User;
+import com.tcser.workshopmongo.resources.util.URL;
 import com.tcser.workshopmongo.services.PostService;
 import com.tcser.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,12 @@ public class PostResource {
     public ResponseEntity<Post> finById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> finByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
